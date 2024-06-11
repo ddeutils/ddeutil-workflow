5d32e61729f4ebd317d71d987d369395434149ed
Korawich Anuttra <korawich.anu@gmail.com>
2024-06-11 23:42:09 +0700

    :construction: refactored: merge run example conf file together.

--- tests/test_base_data.py
+++ tests/test_base_data.py
@@ -7,7 +7,6 @@ def test_read_data(data_path: Path):
     assert YamlFl(path=data_path / "conf/demo/01_connections.yml").read()
     assert YamlFl(path=data_path / "conf/demo/02_datasets.yml").read()
     assert YamlFl(path=data_path / "conf/demo/04_00_pipe_run.yml").read()
-    assert YamlFl(path=data_path / "conf/demo/04_01_pipe_run_params.yml").read()
     assert YamlFl(path=data_path / "conf/demo/04_10_pipe_el.yml").read()
     assert YamlFl(path=data_path / "conf/demo/04_20_pipe_transform.yml").read()
     assert YamlFl(path=data_path / "conf/demo/04_30_pipe_etl.yml").read()
