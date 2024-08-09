49fd45092359c66d84dd01e8bdd759e69d52898e
Korawich Anuttra <korawich.anu@gmail.com>
2024-08-09 14:44:20 +0700

    :test_tube: tests: change name of config name that use for test.

--- tests/test__conf_exist.py
+++ tests/test__conf_exist.py
@@ -3,9 +3,9 @@ from pathlib import Path
 from ddeutil.io.__base import YamlFl
 
 
-def test_read_data(test_path: Path):
-    assert YamlFl(path=test_path / "conf/demo/04_00_pipe_run.yml").read()
-    assert YamlFl(path=test_path / "conf/demo/04_10_pipe_el.yml").read()
-    assert YamlFl(path=test_path / "conf/demo/04_20_pipe_transform.yml").read()
-    assert YamlFl(path=test_path / "conf/demo/04_30_pipe_etl.yml").read()
-    assert YamlFl(path=test_path / "conf/demo/05_schedules.yml").read()
+def test_read_data(conf_path: Path):
+    assert YamlFl(path=conf_path / "demo/04_00_pipe_run.yml").read()
+    assert YamlFl(path=conf_path / "demo/04_10_pipe_task.yml").read()
+    assert YamlFl(path=conf_path / "demo/04_20_pipe_task_metrix.yml").read()
+    assert YamlFl(path=conf_path / "demo/04_30_pipe_trigger.yml").read()
+    assert YamlFl(path=conf_path / "demo/05_schedules.yml").read()
--- tests/test_pipeline_params.py
+++ tests/test_pipeline_params.py
@@ -5,7 +5,7 @@ import ddeutil.workflow.pipeline as pipe
 
 def test_pipe_params_py():
     pipeline = pipe.Pipeline.from_loader(
-        name="transform_proc_mssql",
+        name="ingest_postgres_to_minio",
         externals={},
     )
     rs = pipeline.params["run-date"].receive("2024-01-01")
