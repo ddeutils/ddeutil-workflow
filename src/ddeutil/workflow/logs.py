65b4b3eada71ee3dbe1b993cf8fc5944b354b07a
Korawich Anuttra <korawich.anu@gmail.com>
2025-03-16 11:48:29 +0700

    :recycle: clean: clear code on logs module.

--- src/ddeutil/workflow/logs.py
+++ src/ddeutil/workflow/logs.py
@@ -15,7 +15,7 @@ from datetime import datetime
 from inspect import Traceback, currentframe, getframeinfo
 from pathlib import Path
 from threading import get_ident
-from typing import ClassVar, Optional, Union
+from typing import ClassVar, Literal, Optional, Union
 
 from pydantic import BaseModel, Field
 from pydantic.dataclasses import dataclass
@@ -54,50 +54,104 @@ class BaseTraceLog(ABC):  # pragma: no cov
 
     @abstractmethod
     def writer(self, message: str, is_err: bool = False) -> None:
+        """Write a trace message after making to target pointer object. The
+        target can be anything be inherited this class and overwrite this method
+        such as file, console, or database.
+
+        :param message: A message after making.
+        :param is_err: A flag for writing with an error trace or not.
+        """
         raise NotImplementedError(
             "Create writer logic for this trace object before using."
         )
 
     @abstractmethod
     def make_message(self, message: str) -> str:
+        """Prepare and Make a message before write and log processes.
+
+        :param message: A message that want to prepare and make before.
+
+        :rtype: str
+        """
         raise NotImplementedError(
             "Adjust make message method for this trace object before using."
         )
 
     def debug(self, message: str):
+        """Write trace log with append mode and logging this message with the
+        DEBUG level.
+
+        :param message: (str) A message that want to log.
+        """
         msg: str = self.make_message(message)
 
-        # NOTE: Write file if debug mode.
+        # NOTE: Write file if debug mode was enabled.
         if config.debug:
             self.writer(msg)
 
         logger.debug(msg, stacklevel=2)
 
-    def info(self, message: str):
+    def info(self, message: str) -> None:
+        """Write trace log with append mode and logging this message with the
+        INFO level.
+
+        :param message: (str) A message that want to log.
+        """
         msg: str = self.make_message(message)
         self.writer(msg)
         logger.info(msg, stacklevel=2)
 
-    def warning(self, message: str):
+    def warning(self, message: str) -> None:
+        """Write trace log with append mode and logging this message with the
+        WARNING level.
+
+        :param message: (str) A message that want to log.
+        """
         msg: str = self.make_message(message)
         self.writer(msg)
         logger.warning(msg, stacklevel=2)
 
-    def error(self, message: str):
+    def error(self, message: str) -> None:
+        """Write trace log with append mode and logging this message with the
+        ERROR level.
+
+        :param message: (str) A message that want to log.
+        """
         msg: str = self.make_message(message)
         self.writer(msg, is_err=True)
         logger.error(msg, stacklevel=2)
 
-    def exception(self, message: str):
+    def exception(self, message: str) -> None:
+        """Write trace log with append mode and logging this message with the
+        EXCEPTION level.
+
+        :param message: (str) A message that want to log.
+        """
         msg: str = self.make_message(message)
         self.writer(msg, is_err=True)
         logger.exception(msg, stacklevel=2)
 
 
-class TraceData(BaseModel):
-    stdout: str
-    stderr: str
-    meta: list[dict] = Field(default_factory=dict)
+class TraceMeda(BaseModel):  # pragma: no cov
+    mode: Literal["stdout", "stderr"]
+    datetime: str
+    process: int
+    thread: int
+    message: str
+    filename: str
+    lineno: int
+
+
+class TraceData(BaseModel):  # pragma: no cov
+    stdout: str = Field(description="A standard output trace data.")
+    stderr: str = Field(description="A standard error trace data.")
+    meta: list[TraceMeda] = Field(
+        default_factory=list,
+        description=(
+            "A metadata mapping of this output and error before making it to "
+            "standard value."
+        ),
+    )
 
     @classmethod
     def from_path(cls, file: Path) -> Self:
@@ -169,17 +223,26 @@ class FileTraceLog(BaseTraceLog):  # pragma: no cov
         return f"{cut_parent_run_id} -> {cut_run_id}"
 
     def make_message(self, message: str) -> str:
+        """Prepare and Make a message before write and log processes.
+
+        :param message: A message that want to prepare and make before.
+
+        :rtype: str
+        """
         return f"({self.cut_id}) {message}"
 
     def writer(self, message: str, is_err: bool = False) -> None:
-        """The path of logging data will store by format:
+        """ "Write a trace message after making to target file and write metadata
+        in the same path of standard files.
+
+            The path of logging data will store by format:
 
             ... ./logs/run_id=<run-id>/metadata.json
             ... ./logs/run_id=<run-id>/stdout.txt
             ... ./logs/run_id=<run-id>/stderr.txt
 
-        :param message:
-        :param is_err:
+        :param message: A message after making.
+        :param is_err: A flag for writing with an error trace or not.
         """
         if not config.enable_write_log:
             return
@@ -193,40 +256,24 @@ class FileTraceLog(BaseTraceLog):  # pragma: no cov
         thread: int = get_ident()
 
         write_file: str = "stderr.txt" if is_err else "stdout.txt"
+        write_data: dict[str, Union[str, int]] = {
+            "datetime": get_dt_tznow().strftime(config.log_datetime_format),
+            "process": process,
+            "thread": thread,
+            "message": message,
+            "filename": filename,
+            "lineno": lineno,
+        }
+
         with (self.pointer / write_file).open(mode="at", encoding="utf-8") as f:
             msg_fmt: str = f"{config.log_format_file}\n"
-            f.write(
-                msg_fmt.format(
-                    **{
-                        "datetime": get_dt_tznow().strftime(
-                            config.log_datetime_format
-                        ),
-                        "process": process,
-                        "thread": thread,
-                        "message": message,
-                        "filename": filename,
-                        "lineno": lineno,
-                    }
-                )
-            )
+            f.write(msg_fmt.format(**write_data))
 
         with (self.pointer / "metadata.json").open(
             mode="at", encoding="utf-8"
         ) as f:
             f.write(
-                json.dumps(
-                    {
-                        "mode": write_file.split(".")[0],
-                        "datetime": get_dt_tznow().strftime(
-                            config.log_datetime_format
-                        ),
-                        "process": process,
-                        "thread": thread,
-                        "message": message,
-                        "filename": filename,
-                        "lineno": lineno,
-                    }
-                )
+                json.dumps({"mode": write_file.split(".")[0]} | write_data)
                 + "\n"
             )
 
