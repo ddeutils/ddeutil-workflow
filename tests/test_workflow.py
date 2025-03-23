0f61ba74a73ffe85a3d59c517086726d704eae7f
Korawich Anuttra <korawich.anu@gmail.com>
2025-03-23 09:23:18 +0700

    :gear: fixed: testcase that assert matrix key.

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -208,7 +208,7 @@ def test_workflow_condition(test_path):
         rs: Result = workflow.execute(params={"name": "bar"})
         assert {
             "params": {"name": "bar"},
-            "jobs": {"condition-job": {"matrix": {}, "stages": {}}},
+            "jobs": {"condition-job": {"stages": {}}},
         } == rs.context
 
         rs: Result = workflow.execute(params={"name": "foo"})
