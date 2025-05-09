5da5a3879e904586e697064fc0662750dcbb0057
Korawich Anuttra <korawich.anu@gmail.com>
2025-05-09 19:59:42 +0700

    :test_tube: tests: add testcase for create full example workflow usecase.

---
+++ tests/example/test_stage_example.py
@@ -0,0 +1,27 @@
+import shutil
+from datetime import datetime
+
+from ddeutil.workflow import Result, Workflow
+
+
+def test_example_stage_exec_nested_trigger(test_path):
+    if (test_path / "example/logs").exists():
+        shutil.rmtree(test_path / "example/logs")
+
+    workflow = Workflow.from_conf(
+        "stream-workflow",
+        extras={
+            "trace_path": test_path / "example/logs/trace",
+            "audit_path": test_path / "example/logs/audit",
+            "enable_write_log": True,
+            "enable_write_audit": True,
+        },
+    )
+    rs: Result = workflow.release(
+        datetime(2025, 5, 10, 12, 35),
+        params={
+            "run-mode": "N",
+            "name": "starter-stream-name",
+        },
+    )
+    print(rs)
