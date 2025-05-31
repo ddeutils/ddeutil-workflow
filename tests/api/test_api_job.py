a9082af59402d28be5186d3d96fea5403a8e282e
Korawich Anuttra <korawich.anu@gmail.com>
2025-05-31 12:50:52 +0700

    :test_tube: tests: update testcase for job execute api.

--- tests/api/test_api_job.py
+++ tests/api/test_api_job.py
@@ -7,6 +7,47 @@ def tests_route_job_execute(client):
         json={
             "result": {"run_id": "1234", "parent_run_id": "4567"},
             "job": {
+                "id": "first-job",
+                "stages": [
+                    {"name": "Empty first", "echo": "hello world"},
+                    {"name": "Empty second", "echo": "hello foo"},
+                ],
+            },
+            "params": {},
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "Execute job via RestAPI successful.",
+        "result": {"run_id": "1234", "parent_run_id": "4567"},
+        "job": {
+            "id": "first-job",
+            "stages": [
+                {"name": "Empty first", "echo": "hello world"},
+                {"name": "Empty second", "echo": "hello foo"},
+            ],
+        },
+        "params": {},
+        "context": {
+            "jobs": {
+                "first-job": {
+                    "status": "SUCCESS",
+                    "stages": {
+                        "0207202496": {"outputs": {}, "status": "SUCCESS"},
+                        "0433802983": {"outputs": {}, "status": "SUCCESS"},
+                    },
+                },
+            },
+        },
+    }
+
+
+def tests_route_job_execute_not_pass_job_id(client):
+    response = client.post(
+        f"{api_config.prefix_path}/job/execute/",
+        json={
+            "result": {"run_id": "1234", "parent_run_id": "4567"},
+            "job": {
                 "stages": [
                     {"name": "Empty first", "echo": "hello world"},
                     {"name": "Empty second", "echo": "hello foo"},
@@ -15,9 +56,9 @@ def tests_route_job_execute(client):
             "params": {},
         },
     )
-    assert response.status_code == 200
+    assert response.status_code == 500
     assert response.json() == {
-        "message": "Execute job via RestAPI.",
+        "message": "This job do not set the ID before setting execution output.",
         "result": {"run_id": "1234", "parent_run_id": "4567"},
         "job": {
             "stages": [
