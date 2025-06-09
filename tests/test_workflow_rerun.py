c0a35b73e193e45bd9c13bb0c3cbc0efa3f9791f
Korawich Anuttra <korawich.anu@gmail.com>
2025-06-09 11:09:02 +0700

    :gear: fixed: filter failure job before release rerun job step.

--- tests/test_workflow_rerun.py
+++ tests/test_workflow_rerun.py
@@ -1,4 +1,5 @@
 from ddeutil.workflow import (
+    CANCEL,
     FAILED,
     SUCCESS,
     Job,
@@ -92,5 +93,75 @@ def test_workflow_rerun():
                 "status": SUCCESS,
                 "stages": {"7972360640": {"outputs": {}, "status": SUCCESS}},
             },
+        },
+    }
+
+
+def test_workflow_rerun_parallel_timeout():
+    job: Job = Job(
+        stages=[
+            {"name": "Sleep", "run": "import time\ntime.sleep(2)"},
+            {"name": "Echo Last Stage", "echo": "the last stage"},
+        ],
+    )
+    workflow: Workflow = Workflow(
+        name="demo-workflow",
+        jobs={
+            "sleep-run": job,
+            "sleep-again-run": job.model_copy(update={"needs": ["sleep-run"]}),
+        },
+        extras={"stage_default_id": False},
+    )
+    rs: Result = workflow.rerun(
+        context={
+            "status": FAILED,
+            "params": {},
+            "jobs": {
+                "sleep-run": {
+                    "status": CANCEL,
+                    "stages": {},
+                    "errors": {
+                        "name": "JobCancelError",
+                        "message": (
+                            "Strategy execution was canceled from the event before "
+                            "start stage execution."
+                        ),
+                    },
+                },
+            },
+            "errors": {
+                "name": "WorkflowTimeoutError",
+                "message": (
+                    "'demo-workflow' was timeout because it use exec time more "
+                    "than 1.25 seconds."
+                ),
+            },
+        },
+        timeout=1.25,
+        max_job_parallel=2,
+    )
+    assert rs.status == FAILED
+    assert rs.context == {
+        "status": FAILED,
+        "params": {},
+        "jobs": {
+            "sleep-run": {
+                "status": CANCEL,
+                "stages": {},
+                "errors": {
+                    "name": "JobCancelError",
+                    "message": (
+                        "Strategy execution was canceled from the event before "
+                        "start stage execution."
+                    ),
+                },
+            },
+        },
+        "errors": {
+            "name": "WorkflowTimeoutError",
+            "message": (
+                "'demo-workflow' was timeout because it use exec time more "
+                "than 1.25 seconds."
+            ),
         },
     }
